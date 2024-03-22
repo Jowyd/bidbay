5a965d0d1eb875084e585bdbbe6a95134e0dd484
Lyam <lyamkeovilay74@gmail.com>
2024-03-22 11:09:22 +0100

    route user/:userId

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -4,7 +4,29 @@ import { User, Product, Bid } from '../orm/index.js'
 const router = express.Router()
 
 router.get('/api/users/:userId', async (req, res) => {
-  res.status(600).send()
+  const userId = req.params.userId
+  const user = await User.findByPk(req.params.userId)
+  const products = await Product.findAll({
+    where: {
+      sellerId: userId
+      }
+      });
+  const bids  = await Bid.findAll({
+    where: {
+      bidderId: userId
+      },
+    include: Product
+      });
+  res.send({user, products, bids});
+})
+
+router.get("/api/users/:userId/products", async (req, res) => {
+  const result = await Product.findAll({
+    where: {
+      userId: req.params.userId
+    }
+  })
+  res.send(result);
 })
 
 export default router
