33c12ba5b7d2a7de7ecd41a92322b2a02c1db81d
Natalia <natalia.surdu@proton.me>
2024-04-08 10:30:17 +0200

    test

--- frontend/cypress/e2e/meta/authors.cy.js
+++ frontend/cypress/e2e/meta/authors.cy.js
@@ -3,8 +3,8 @@ import { authors, group } from "../../../meta/authors";
 describe("Meta", () => {
   it("Remplisser les infos de votre groupe dans frontend/meta/authors.js", () => {
     expect(authors).to.not.deep.eq([
-      "John Doe (p2203293)",
-      "Jane Doe (p2312523)",
+      "Natalia Surdu (12312523)",
+      "Lyam Keovilay (12203293)",
     ]);
 
     expect(group).to.oneOf([1, 2, 3]);
