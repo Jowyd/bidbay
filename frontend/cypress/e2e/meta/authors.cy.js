37b8fc553648d8d64515997ca62b9f341d34ff0e
Natalia <natalia.surdu@proton.me>
2024-04-08 11:09:46 +0200

    gygu

--- frontend/cypress/e2e/meta/authors.cy.js
+++ frontend/cypress/e2e/meta/authors.cy.js
@@ -3,8 +3,8 @@ import { authors, group } from "../../../meta/authors";
 describe("Meta", () => {
   it("Remplisser les infos de votre groupe dans frontend/meta/authors.js", () => {
     expect(authors).to.not.deep.eq([
-      "Natalia Surdu (12312523)",
-      "Lyam Keovilay (12203293)",
+      "John Doe (p12345678)",
+      "Jane Doe (p12345678)",
     ]);
 
     expect(group).to.oneOf([1, 2, 3]);
--- frontend/src/views/ProductView.vue
+++ frontend/src/views/ProductView.vue
@@ -240,11 +240,10 @@ const submitBid = async () => {
           <button
             type="submit"
             class="btn btn-primary"
-            v-bind:disabled="bidAmount < 10 || product?.sellerId === userData?.id" 
+            v-bind:disabled="bidAmount < 10 || bidAmount <= (product?.bids[product.bids.length-1]?.price ?? 0) ||product?.sellerId === userData?.id" 
             data-test-submit-bid
             @click="submitBid()"
           >
-          <!-- bidAmount <= (product?.bids[product.bids.length-1]?.price ?? 0) ||  -->
             Enchérir
           </button>
         </form>
