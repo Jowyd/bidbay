4ee4916f4e103167c9aff0265f129c141344851f
Natalia <natalia.surdu@proton.me>
2024-04-08 09:51:35 +0200

    add product all tests passed

--- frontend/src/views/AddProductView.vue
+++ frontend/src/views/AddProductView.vue
@@ -43,52 +43,28 @@ async function addProduct() {
   <div class="row justify-content-center">
     <div class="col-md-6">
       <form @submit.prevent="addProduct">
-        <div
-          class="alert alert-danger mt-4"
-          role="alert"
-          data-test-error
-          v-if="errorMessage != ''"
-        >
+        <div class="alert alert-danger mt-4" role="alert" data-test-error v-if="errorMessage != ''">
           Une erreur s'est produite {{ errorMessage }}
         </div>
 
         <div class="mb-3">
           <label for="product-name" class="form-label"> Nom du produit </label>
-          <input
-            type="text"
-            class="form-control"
-            id="product-name"
-            v-model="product.name"
-            required
-            data-test-product-name
-          />
+          <input type="text" class="form-control" id="product-name" v-model="product.name" required
+            data-test-product-name />
         </div>
 
         <div class="mb-3">
           <label for="product-description" class="form-label">
             Description
           </label>
-          <textarea
-            class="form-control"
-            id="product-description"
-            name="description"
-            rows="3"
-            required
-            data-test-product-description
-            v-model="product.description"
-          ></textarea>
+          <textarea class="form-control" id="product-description" name="description" rows="3" required
+            data-test-product-description v-model="product.description"></textarea>
         </div>
 
         <div class="mb-3">
           <label for="product-category" class="form-label"> Catégorie </label>
-          <input
-            type="text"
-            class="form-control"
-            id="product-category"
-            required
-            data-test-product-category
-            v-model="product.category"
-          />
+          <input type="text" class="form-control" id="product-category" required data-test-product-category
+            v-model="product.category" />
         </div>
 
         <div class="mb-3">
@@ -96,17 +72,8 @@ async function addProduct() {
             Prix de départ
           </label>
           <div class="input-group">
-            <input
-              type="number"
-              class="form-control"
-              id="product-original-price"
-              name="originalPrice"
-              step="1"
-              min="0"
-              required
-              data-test-product-price
-              v-model="product.originalPrice"
-            />
+            <input type="number" class="form-control" id="product-original-price" name="originalPrice" step="1" min="0"
+              required data-test-product-price v-model="product.originalPrice" />
             <span class="input-group-text">€</span>
           </div>
         </div>
@@ -115,47 +82,23 @@ async function addProduct() {
           <label for="product-picture-url" class="form-label">
             URL de l'image
           </label>
-          <input
-            type="url"
-            class="form-control"
-            id="product-picture-url"
-            name="pictureUrl"
-            required
-            data-test-product-picture
-            v-model="product.pictureUrl"
-          />
+          <input type="url" class="form-control" id="product-picture-url" name="pictureUrl" required
+            data-test-product-picture v-model="product.pictureUrl" />
         </div>
 
         <div class="mb-3">
           <label for="product-end-date" class="form-label">
             Date de fin de l'enchère
           </label>
-          <input
-            type="date"
-            class="form-control"
-            id="product-end-date"
-            name="endDate"
-            required
-            data-test-product-end-date
-            v-model="product.endDate"
-          />
+          <input type="date" class="form-control" id="product-end-date" name="endDate" required
+            data-test-product-end-date v-model="product.endDate" />
         </div>
 
         <div class="d-grid gap-2">
-          <button
-            type="submit"
-            class="btn btn-primary"
-            :disabled="loading"
-            data-test-submit
-          >
+          <button type="submit" class="btn btn-primary" :disabled="loading" data-test-submit>
             Ajouter le produit
-            <span
-              data-test-spinner
-              class="spinner-border spinner-border-sm"
-              role="status"
-              aria-hidden="true"
-              v-if="loading"
-            ></span>
+            <span data-test-spinner class="spinner-border spinner-border-sm" role="status" aria-hidden="true"
+              v-if="loading"></span>
           </button>
         </div>
       </form>
